6db3b99cc64e2daf94cfb90d91c8e88d8ba5ec4e
wesley <wesleydsilva96@gmail.com>
2025-01-22 15:48:55 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.devsuperior.dslist;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
