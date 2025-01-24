9280c5c4c02c34b2c6b5fea43cf4277f3893124a
wesley <wesleydsilva96@gmail.com>
2025-01-24 14:16:32 -0300

    organizando as pastas

--- src/main/java/com/devsuperior/dslist/GameListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.dslist;
-
-import com.devsuperior.dslist.entity.GameList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameListRepository extends JpaRepository<GameList,Long> {
-}
--- src/main/java/com/devsuperior/dslist/WebConfig.java
+++
@@ -1,2 +0,0 @@
-package com.devsuperior.dslist;public class WebConfig {
-}
